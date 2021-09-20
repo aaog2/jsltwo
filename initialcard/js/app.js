5bce323774f62fb38f9799c8a2d73b2381aee044
Aung Aung Oo <aungaungoogame2@gmail.com>
2021-09-20 21:46:46 +0630

    20/9/2021

---
+++ initialcard/js/app.js
@@ -0,0 +1,13 @@
+// UI
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach(panel=>{
+    panel.addEventListener('click',()=>{
+        removeactivepreclass();
+        panel.classList.add("active");
+    });
+});
+
+function removeactivepreclass(){
+    panels.forEach(panel=>panel.classList.remove("active"));
+}
