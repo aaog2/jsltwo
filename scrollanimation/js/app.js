eb87966f45907f34a41626597012d92b5ec0ea6d
Aung Aung Oo <aungaungoogame2@gmail.com>
2021-10-12 11:54:56 +0630

    12/10/2021

--- scrollanimation/js/app.js
+++ scrollanimation/js/app.js
@@ -1,1 +1,27 @@
-console.log("hey");
+// UI
+const boxs = document.querySelectorAll('.box');
+// console.log(boxs);
+checkboxes();
+
+window.addEventListener("scroll",checkboxes);
+
+function checkboxes(){
+
+    let innerheight = window.innerHeight / 1.5; //1.5 nal sarr tr ka hwat twr tr ko myin chin lo
+
+    console.log(innerheight);
+
+    boxs.forEach(box=>{
+
+        const boxtop = box.getBoundingClientRect().top;
+
+        console.log(boxtop);
+
+        if(boxtop < innerheight){
+            box.classList.add('show');
+        }else {
+            box.classList.remove("show");
+        }
+
+    })
+}
